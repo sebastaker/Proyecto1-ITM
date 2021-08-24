45c5d73a542ff8460b76eeccd2c6065bd80b2a9f
SEBASTIAN ALZATE LOAIZA <sebastianalzate82693@correo.itm.edu.co>
2021-08-24 02:28:37 -0500

    Se agregaron entidades

---
+++ Proyecto1-ITM.Common/Models/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Proyecto1_ITM.Common.Models
+{
+    public class Todo
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ Proyecto1-ITM.Common/Responses/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto1_ITM.Common.Responses
+{
+    public class Response
+    {
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; }
+
+        public object Result { get; set; }
+    }
+}
